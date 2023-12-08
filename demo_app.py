befb3c4883907b3e1bb4242c1735a418a8434c55
Harsh Kumar <harsh.kumar@stylumia.com>
2023-12-08 12:59:31 +0530

    123

--- demo_app.py
+++ demo_app.py
@@ -11,3 +11,5 @@ print('testing normal merge  , commiting in master')
 print('testing normal merge  , commiting in master again')
 
 print('testing nromal merge , commited in branch named as "branch_to_test_normal_merge"')
+
+print('yoyo')

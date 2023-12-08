d35d9e1321a0a8f6d7ef07f736ee2cc5ab785a6d
Harsh Kumar <harsh.kumar@stylumia.com>
2023-12-08 12:08:06 +0530

    this is test

--- demo_app.py
+++ demo_app.py
@@ -7,3 +7,5 @@ st.sidebar.header('Side Bar')
 st.text('new_text')
 
 print('testing normal merge  , commiting in master')
+
+print('testing nromal merge , commited in branch named as "branch_to_test_normal_merge"')
